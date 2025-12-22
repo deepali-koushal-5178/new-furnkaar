7096d0cbead1fa96ef4efba05bd57744b960e5d4
deepali <deepalikoushal5178@gmail.com>
2025-12-22 14:20:16 +0530

    hindi and en folder removegetting error while uploading on vercel

--- app/hindi/head.js
+++
@@ -1,7 +0,0 @@
-import hi from "../../locales/hi.json";
-export default function Head() {
-  return <>
-    <title>{hi.siteTitle}</title>
-    <meta name="description" content={hi.metaDescription || ""} />
-  </>;
-}
--- app/hindi/page.js
+++
@@ -1,7 +0,0 @@
-import ClientHome from "@/components/ClientHome";
-import { getDictionary } from "@/lib/getDictionary";
-
-export default async function HindiPage() {
-  const dict = await getDictionary("hi");
-  return <ClientHome dict={dict} lang="hi" />;
-}

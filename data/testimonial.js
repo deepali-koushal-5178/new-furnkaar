583230f97946bb78c12159f46a3204ee6e61af9a
deepali <deepalikoushal5178@gmail.com>
2025-12-22 15:05:52 +0530

    Connect local project to existing repo

---
+++ data/testimonial.js
@@ -0,0 +1,20 @@
+export const testimonialsData = [
+    {
+      quote:
+        "Loved the precise finish and the zero-mess installation experience.",
+      name: "Anjali Raikwar",
+      location: "Indore",
+    },
+    {
+      quote:
+        "Beautiful modular furniture with great quality and smooth process.",
+      name: "Ravi Sharma",
+      location: "Indore",
+    },
+    {
+      quote:
+        "Professional team, timely delivery, and excellent craftsmanship.",
+      name: "Neha Trivedi",
+      location: "Ujjain",
+    },
+  ];
